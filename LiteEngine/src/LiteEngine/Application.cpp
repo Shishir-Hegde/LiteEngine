cb3652b7fe1ad0a46710a32dfe1386c4dff70e7f
Tanmay <tanmaygaurtg28@gmail.com>
2026-02-01 11:44:02 +0530

    Experimental commit

--- LiteEngine/src/LiteEngine/Application.cpp
+++ LiteEngine/src/LiteEngine/Application.cpp
@@ -1,26 +1,46 @@
 #include "hzpch.h"
 #include "Application.h"
-#include "Events/ApplicationEvent.h"
-#include "Log.h"
+
+#include "LiteEngine/Log.h"
+
 #include <GLFW/glfw3.h>
 
-namespace LiteEngine
-{
+namespace LiteEngine {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
+
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		LE_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
 	{
 		while (m_Running)
 		{
-			glClearColor(1, 0, 0, 1);
+			glClearColor(1, 0, 1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
 	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
 }
--- LiteEngine/src/LiteEngine/Application.h
+++ LiteEngine/src/LiteEngine/Application.h
@@ -2,6 +2,7 @@
  
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 #include "Windows.h"
 
 namespace LiteEngine
@@ -13,7 +14,9 @@ namespace LiteEngine
 		virtual ~Application();
 
 		void Run();
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
