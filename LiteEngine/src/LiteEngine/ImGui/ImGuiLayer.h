a816236f2642028ebcd725e45dea51e2889a455b
shyamuraicoder <shyamuraicoder31@gmail.com>
2026-03-12 02:11:10 +0530

    Added Events to imgui layer

--- LiteEngine/src/LiteEngine/ImGui/ImGuiLayer.h
+++ LiteEngine/src/LiteEngine/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "LiteEngine/Layer.h"
 
+#include "LiteEngine/Events/ApplicationEvent.h"
+#include "LiteEngine/Events/KeyEvent.h"
+#include "LiteEngine/Events/MouseEvent.h"
+
 namespace LiteEngine {
 
 	class LITEENGINE_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace LiteEngine {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
