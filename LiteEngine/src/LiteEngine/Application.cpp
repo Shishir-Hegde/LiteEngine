230abeb2ce9d1e9d2de05bf24440ce8f02c62b94
shyamuraicoder <shyamuraicoder31@gmail.com>
2026-03-13 11:47:28 +0530

    Added Input Polling

--- LiteEngine/src/LiteEngine/Application.cpp
+++ LiteEngine/src/LiteEngine/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace LiteEngine {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -60,6 +62,9 @@ namespace LiteEngine {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			LE_CORE_TRACE("({0}, {1})", x, y);
 
 			m_Window->OnUpdate();
 		}
