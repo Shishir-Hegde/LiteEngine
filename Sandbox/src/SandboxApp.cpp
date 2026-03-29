63ff71def9ac7e2557960d154426c869bf59357a
shyamuraicoder <shyamuraicoder31@gmail.com>
2026-03-29 15:00:27 +0530

    Added transforms to Renderer.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,5 +1,6 @@
 #include "LiteEngine.h"
 #include "imgui/imgui.h"
+#include <glm/gtc/matrix_transform.hpp>
 #include "LiteEngine/Events/Event.h"
 #include "LiteEngine/Log.h"
 #include "spdlog/fmt/ostr.h"
@@ -41,10 +42,10 @@ public:
 	  m_SquareVA.reset(LiteEngine::VertexArray::Create());
 
 	  float squareVertices[3 * 4] = {
-		  -0.75f, -0.75f, 0.0f,
-		   0.75f, -0.75f, 0.0f,
-		   0.75f,  0.75f, 0.0f,
-		  -0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 	  };
 
 	  std::shared_ptr<LiteEngine::VertexBuffer> squareVB;
@@ -66,6 +67,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -74,7 +76,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -101,13 +103,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -153,7 +156,17 @@ public:
 
 	  LiteEngine::Renderer::BeginScene(m_Camera);
 
-	  LiteEngine::Renderer::Submit(m_BlueShader, m_SquareVA);
+	  glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+	  for (int y = 0; y < 20; y++)
+	  {
+		  for (int x = 0; x < 20; x++)
+		  {
+			  glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+			  glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+			  LiteEngine::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+		  }
+	  }
 	  LiteEngine::Renderer::Submit(m_Shader, m_VertexArray);
 
 	  LiteEngine::Renderer::EndScene();
