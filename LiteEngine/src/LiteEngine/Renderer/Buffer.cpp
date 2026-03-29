561c1e464c3897ca9a3b587d9ab253c6c05d9ca6
shyamuraicoder <shyamuraicoder31@gmail.com>
2026-03-29 13:28:50 +0530

    Added RendererAPI and RenderCommand architecture.

--- LiteEngine/src/LiteEngine/Renderer/Buffer.cpp
+++ LiteEngine/src/LiteEngine/Renderer/Buffer.cpp
@@ -9,8 +9,8 @@ namespace LiteEngine {
 	{
 		switch (Renderer::GetAPI())
 		{
-		case RendererAPI::None:    LE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::OpenGL:  return new OpenGLVertexBuffer(vertices, size);
+		case RendererAPI::API::None:    LE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return new OpenGLVertexBuffer(vertices, size);
 		}
 
 		LE_CORE_ASSERT(false, "Unknown RendererAPI!");
@@ -21,8 +21,8 @@ namespace LiteEngine {
 	{
 		switch (Renderer::GetAPI())
 		{
-		case RendererAPI::None:    LE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::OpenGL:  return new OpenGLIndexBuffer(indices, size);
+		case RendererAPI::API::None:    LE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return new OpenGLIndexBuffer(indices, size);
 		}
 
 		LE_CORE_ASSERT(false, "Unknown RendererAPI!");
--- LiteEngine/src/LiteEngine/Renderer/Renderer.h
+++ LiteEngine/src/LiteEngine/Renderer/Renderer.h
@@ -1,18 +1,18 @@
 #pragma once
 
-namespace LiteEngine {
+#include "RenderCommand.h"
 
-	enum class RendererAPI
-	{
-		None = 0, OpenGL = 1
-	};
+namespace LiteEngine {
 
 	class Renderer
 	{
 	public:
-		inline static RendererAPI GetAPI() { return s_RendererAPI; }
-	private:
-		static RendererAPI s_RendererAPI;
+		static void BeginScene();
+		static void EndScene();
+
+		static void Submit(const std::shared_ptr<VertexArray>& vertexArray);
+
+		inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
 	};
 
 }
