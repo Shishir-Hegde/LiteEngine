202c118fc0854b662aa68845d781b1851d3033e3
shyamuraicoder <shyamuraicoder31@gmail.com>
2026-03-29 11:25:45 +0530

    Added vertex array support.

--- LiteEngine/src/LiteEngine/Application.h
+++ LiteEngine/src/LiteEngine/Application.h
@@ -10,6 +10,7 @@
 #include "LiteEngine/ImGui/ImGuiLayer.h"
 #include "LiteEngine/Renderer/Shader.h"
 #include "LiteEngine/Renderer/Buffer.h"
+#include "LiteEngine/Renderer/VertexArray.h"
 
 struct ImGuiContext;
 
@@ -40,10 +41,11 @@ namespace LiteEngine {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 	private:
 		static Application* s_Instance;
 	};
